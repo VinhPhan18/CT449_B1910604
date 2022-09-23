3651274564b812414c1b36dc960e0ad7375830d7
VinhPhan18 <111651546+VinhPhan18@users.noreply.github.com>
2022-09-23 16:29:44 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 const express = require("express");
 const cors = require("cors");
-const contactsRouter = require("./app/routes/contact.route")
-
+const contactsRouter = this.require("./app/routes/contact.route");
+const ApiError = require("./app/api-error");
 const app = express();
-
 app.use(cors());
 app.use(express.json());
 app.use("/api/contacts", contactsRouter);
-
-app.get("/", (req, res) => {
-   res.json({message: "Welcome to contact book application."});
+app.use((req, res, net) => {
+    return next(new ApiError(404, "Resource not found"));
 });
 
+app.get("/", (req, res)=>{
+    res.json({ message: " Welcome to contact book application."
+});
+});
+app.use("/api/contacts", contactsRouter);
+app.use((err, req, res, next) => {
+    return res.status(error.statusCode || 500).json({
+        message: error.message || "Internal serveer Error",
+    });
+});
 module.exports = app;
